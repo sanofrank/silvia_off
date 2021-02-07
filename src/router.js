acfc014c47b866ac36c745f4476a435ad84e154e
Francesco <francesco.paolucci.93@gmail.com>
2021-02-07 10:13:46 +0100

    routing regex

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import App from './App'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {path: '/', component: App},
+        {path: '/:construction(.*)', redirect: '/'}
+    ]
+})
+
+export default router;
